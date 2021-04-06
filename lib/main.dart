864106874f5d4d5a23e8755159bda0da8800b4ea
Sarah Otto <otto3@hs-mittweida.de>
2021-04-06 18:29:17 +0200

    revocation, new examples

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,129 @@ void main(List<String> args) {
     ..addCommand(WalletCommand())
     ..addCommand(SignatureCommand())
     ..addCommand(Erc1056Command())
+    ..addCommand(RevocationCommand())
     ..run(args);
+}
+
+class RevocationCommand extends Command {
+  final name = 'revocation';
+  final description = 'Interaction with Revocation registry Contract';
+
+  RevocationCommand() {
+    argParser
+      ..addOption('rpcUrl',
+          defaultsTo: 'http://localhost:8545',
+          help: 'Url for RPC-Endpoint of Ethereum-Node')
+      ..addOption('revocationContract',
+          abbr: 'r', help: 'Contract address of Revocation-Contract')
+      ..addFlag('init',
+          abbr: 'i',
+          help:
+              'Initialize (deploy) new Revocation-Registry-Contract to Ethereum')
+      ..addOption('isRevoked',
+          help: 'Id of credential thats revocation status should be checked')
+      ..addOption('revoke', help: 'id of credential that should be revoked')
+      ..addOption('wallet',
+          abbr: 'w',
+          help: 'Path to a Wallet the signing keys are in',
+          defaultsTo: 'ssi_wallet')
+      ..addOption('password', abbr: 'p', help: 'Password of the wallet')
+      ..addOption('did',
+          abbr: 'd',
+          help:
+              'The did of which the private key should be used to authorize deployment and revocation operations. If this is not given, the standard-issuer-key is used');
+  }
+
+  run() async {
+    //Initialize new Revocation Registry
+    if (argResults['init']) {
+      var registry = RevocationRegistry(argResults['rpcUrl']);
+      var wallet = await _openWallet();
+      String privKey = await _getPrivateKey(wallet);
+      try {
+        var address = await registry.deploy(privKey);
+        stdout.writeln(address);
+        exit(0);
+      } catch (e) {
+        stderr.writeln('Unable to deploy');
+        stderr.writeln(e);
+        exit(2);
+      }
+    }
+
+    if (argResults['revocationContract'] == null) {
+      stderr.writeln('no contract address for Revocation contract given');
+      exit(2);
+    }
+    var registry = RevocationRegistry(argResults['rpcUrl'],
+        contractAddress: argResults['revocationContract']);
+
+    // Check for Revocation
+    if (argResults['isRevoked'] != null) {
+      try {
+        var res = await registry.isRevoked(argResults['isRevoked']);
+        stdout.writeln(res);
+        exit(0);
+      } catch (e) {
+        stderr.writeln('Unable to call revocation Registry');
+        stderr.writeln(e);
+        exit(2);
+      }
+    }
+
+    //Revoke a credential
+    if (argResults['revoke'] != null) {
+      var wallet = await _openWallet();
+      var privKey = await _getPrivateKey(wallet);
+      try {
+        await registry.revoke(privKey, argResults['revoke']);
+        stdout.writeln('Credential revoked');
+        exit(0);
+      } catch (e) {
+        stderr.writeln('Unable to call revocation Registry');
+        stderr.writeln(e);
+        exit(2);
+      }
+    }
+  }
+
+  Future<WalletStore> _openWallet() async {
+    if (argResults['password'] == null) {
+      stderr.writeln('Wallet-Password missing');
+      exit(2);
+    }
+    var wallet = WalletStore(argResults['wallet']);
+    await wallet.openBoxes(argResults['password']);
+    return wallet;
+  }
+
+  Future<String> _getPrivateKey(WalletStore wallet) async {
+    String privKey;
+    if (argResults['did'] == null) {
+      privKey = wallet.getStandardIssuerPrivateKey();
+      if (privKey == null) {
+        await wallet.initializeIssuer();
+        privKey = wallet.getStandardIssuerPrivateKey();
+      }
+    } else {
+      privKey = wallet.getPrivateKeyToCredentialDid(argResults['did']);
+      if (privKey == null) {
+        privKey = wallet.getPrivateKeyToConnectionDid(argResults['did']);
+      }
+    }
+
+    if (privKey == null) {
+      stderr.writeln('Could not find a private Key');
+      exit(2);
+    }
+    return privKey;
+  }
 }
 
 class Erc1056Command extends Command {
   final name = 'didRegistry';
   final description =
-      'Interacting with an instance of erc1056 - Contract (EthereumDIDIRegistry)';
+      'Interacting with an instance of erc1056 - Contract (EthereumDIDRegistry)';
 
   Erc1056Command() {
     argParser
@@ -92,7 +208,10 @@ class WalletCommand extends Command {
       ..addOption('password',
           abbr: 'p', help: 'Password the wallet is secured with')
       ..addFlag('generateConnectionDid',
-          abbr: 'c', help: 'generates a new DID for a Connection');
+          abbr: 'c', help: 'generates a new DID for a Connection')
+      ..addOption('mnemonic',
+          abbr: 'm', help: 'restore a wallet with that mnemonic')
+      ..addFlag('list', abbr: 'l', help: 'list all connections');
   }
 
   run() async {
@@ -115,6 +234,16 @@ class WalletCommand extends Command {
       }
     }
 
+    if (argResults['mnemonic'] != null) {
+      try {
+        var mn = wallet.initialize(argResults['mnemonic']);
+        stdout.writeln('Successfully restored wallet with mnemonic: $mn');
+      } catch (e) {
+        stderr.writeln(e);
+        exit(2);
+      }
+    }
+
     if (argResults['generateConnectionDid']) {
       try {
         var did = await wallet.getNextConnectionDID();
@@ -123,6 +252,11 @@ class WalletCommand extends Command {
         stderr.writeln(e);
         exit(2);
       }
+    }
+
+    if (argResults['list']) {
+      var all = wallet.getAllConnections();
+      stdout.writeln(all.keys.toList());
     }
 
     await wallet.closeBoxes();
@@ -155,21 +289,20 @@ class SignatureCommand extends Command {
           help: 'Url for RPC-Endpoint of Ethereum-Node')
       ..addOption('erc1056Contract',
           abbr: 'e',
-          help: 'Contract address of ErC1056-Contract (EthereumDIDRegistry)');
+          help: 'Contract address of Erc1056-Contract (EthereumDIDRegistry)');
   }
 
   run() async {
-    if (argResults['password'] == null) {
-      stderr.writeln('Wallet password missing');
-      exit(2);
-    }
-
-    var wallet = WalletStore(argResults['wallet']);
-    await wallet.openBoxes(argResults['password']);
-
     if (argResults['sign']) {
+      if (argResults['password'] == null) {
+        stderr.writeln('Wallet password missing');
+        exit(2);
+      }
+      var wallet = WalletStore(argResults['wallet']);
+      await wallet.openBoxes(argResults['password']);
       var sig = signString(wallet, argResults['did'], argResults['message']);
       stdout.writeln(sig);
+      exit(0);
     }
 
     if (argResults['verify']) {
@@ -181,7 +314,6 @@ class SignatureCommand extends Command {
               contractAddress: argResults['erc1056Contract']);
         } catch (e) {
           stderr.writeln(e);
-          await wallet.closeBoxes();
           exit(2);
         }
       }
@@ -194,7 +326,6 @@ class SignatureCommand extends Command {
           stdout.writeln(result);
         } catch (e) {
           stderr.writeln(e);
-          await wallet.closeBoxes();
           exit(2);
         }
       } else {
@@ -204,13 +335,10 @@ class SignatureCommand extends Command {
           stdout.writeln(result);
         } catch (e) {
           stderr.writeln(e);
-          await wallet.closeBoxes();
           exit(2);
         }
       }
     }
-
-    await wallet.closeBoxes();
     exit(0);
   }
 }
